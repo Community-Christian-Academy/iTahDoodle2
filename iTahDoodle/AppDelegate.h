a654683b8b97e7cd616addee8aec6834ec61879e
Logan Browning <logan@Logans-MacBook-Air.local>
2015-01-30 08:27:36 -0600

    added extra properties and a helper function

--- iTahDoodle/AppDelegate.h
+++ iTahDoodle/AppDelegate.h
@@ -8,9 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+// Declare a helper function that we will use to get a path
+// to the location on disk where we can save the to-do list
+NSString *BNRDocPath(void);
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UITableView *taskTable;
+@property (strong, nonatomic) UITextField *taskField;
+@property (strong, nonatomic) UIButton *insertButton;
+@property (strong, nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
 
 
 @end
