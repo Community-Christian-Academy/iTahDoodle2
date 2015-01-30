f61a1d6ae5754b483e30a7825de686b38a509668
J. Campbell <fork42@mac.com>
2015-01-30 07:54:55 -0600

    Initial Commit

---
+++ iTahDoodle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTahDoodle
+//
+//  Created by Jay Campbell on 1/30/15.
+//  Copyright (c) 2015 CCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
