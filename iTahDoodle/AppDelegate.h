de38aa9aca41eb349d129478141902e19afdbc08
tannerhagood <tannerhagood@me.com>
2015-02-02 17:27:56 -0600

    Made all changes necessary for chapter 31. iTahDoodle ran successfully in the simulator.

--- iTahDoodle/AppDelegate.h
+++ iTahDoodle/AppDelegate.h
@@ -6,12 +6,23 @@
 //  Copyright (c) 2015 CCA. All rights reserved.
 //
 
+
+#import "AppDelegate.h"
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+// Declare a helper function that we will use to get a path
+// to the location on disk where we can save the to-do list
+NSString *BNRDocPath(void);
+
+
+@interface BNRAppDelegate : UIResponder
+<UIApplicationDelegate, UITableViewDataSource>
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UITableView *taskTable;
+@property (strong, nonatomic) UITextField *taskField;
+@property (strong, nonatomic) UIButton *insertButton;
 
+@property (strong, nonatomic) NSMutableArray *tasks; - (void)addTask:(id)sender;
 
 @end
-
--- iTahDoodle/AppDelegate.m
+++ iTahDoodle/AppDelegate.m
@@ -7,17 +7,145 @@
 //
 
 #import "AppDelegate.h"
+#import <UIKit/UIKit.h>
 
-@interface AppDelegate ()
 
+// Helper function to fetch the path to our to-do data stored on disk
+NSString *BNRDocPath()
+{
+    NSArray *pathList = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                            NSUserDomainMask,
+                                                            YES);
+    return [pathList[0] stringByAppendingPathComponent:@"data.td"];
+}
+
+
+@interface BNRAppDelegate ()
 @end
+@implementation BNRAppDelegate
 
-@implementation AppDelegate
+#pragma mark - Application delegate callbacks
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch. Extra stuff.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Attempt to load an existing to-do dataset from an aray stored to disk.
+    NSArray *plist = [NSArray arrayWithContentsOfFile:BNRDocPath()];
+    if (plist) {
+        // If there was a dataset available, copy it into our instance variable.
+        self.tasks = [plist mutableCopy];
+    } else {
+        // Otherwise, just create an empty one to get us started.
+        self.tasks = [NSMutableArray array];
+    }
+    
+    // Is tasks empty?
+    if ([self.tasks count] == 0) {
+        // Put some strings in it
+        [self.tasks addObject:@"Walk the dogs"];
+        [self.tasks addObject:@"Feed the hogs"];
+        [self.tasks addObject:@"Chop the logs"];
+    }
+    
+    // Create and configure the UIWindow instance
+    // A CGRect is a struct with an origin (x,y) and a size (width,height)
+    CGRect windowFrame = [[UIScreen mainScreen] bounds];
+    UIWindow *theWindow = [[UIWindow alloc] initWithFrame:windowFrame];
+    self.window = theWindow;
+    // Define the frame rectangles of the three UI elements
+    // CGRectMake() creates a CGRect from (x, y, width, height)
+    CGRect tableFrame = CGRectMake(0, 80, 320, windowFrame.size.height - 100);
+    CGRect fieldFrame = CGRectMake(20, 40, 200, 31);
+    CGRect buttonFrame = CGRectMake(228, 40, 72, 31);
+    // Create and configure the table view
+    self.taskTable = [[UITableView alloc] initWithFrame:tableFrame
+                                                  style:UITableViewStylePlain];
+    self.taskTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    
+    // Make this object the table view's dataSource
+    self.taskTable.dataSource = self;
+    // Tell the table view which class to instantiate whenever it
+    // needs to create a new cell. This will come in handy if we ever
+    // want to have different kinds of cells in our table.
+    [self.taskTable registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"Cell"];
+    
+    
+    // Create and configure the text field where new tasks will be typed
+    self.taskField = [[UITextField alloc] initWithFrame:fieldFrame];
+    self.taskField.borderStyle = UITextBorderStyleRoundedRect;
+    self.taskField.placeholder = @"Type a task, tap Insert";
+    // Create and configure a rounded rect Insert button
+    self.insertButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.insertButton.frame = buttonFrame;
+    // Buttons behave using a target/action callback
+    // Configure the Insert button's action to call this object's -addTask: method
+    [self.insertButton addTarget:self
+                          action:@selector(addTask:)
+                forControlEvents:UIControlEventTouchUpInside];
+    // Give the button a title
+    [self.insertButton setTitle:@"Insert"
+                       forState:UIControlStateNormal];
+    // Add our three UI elements to the window
+    [self.window addSubview:self.taskTable];
+    [self.window addSubview:self.taskField];
+    [self.window addSubview:self.insertButton];
+    // Finalize the window and put it on the screen
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
+}
+
+#pragma mark - Table View management
+- (NSInteger)tableView:(UITableView *)tableView
+numberOfRowsInSection:(NSInteger)section
+{
+    // Because this table view only has one section,
+    // the number of rows in it is equal to the number
+    // of items in our tasks array
+    return [self.tasks count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // To improve performance, we reconfigure cells in memory
+    // that have scrolled off the screen and hand them back
+    // with new contents instead of always creating new cells.
+    // We ask the table view to check for a reusable cell and,
+    // if none are available, to create one for us to configure.
+    UITableViewCell *c = [self.taskTable dequeueReusableCellWithIdentifier:@"Cell"];
+    // Then we (re)configure the cell based on the model object,
+    // in this case our todoItems array
+    NSString *item = self.tasks[indexPath.row];
+    c.textLabel.text = item;
+    // and hand back to the table view the properly configured cell
+    return c;
+}
+
+#pragma mark - Actions
+- (void)addTask:(id)sender
+{
+    // Get the to-do item
+    NSString *txt = [self.taskField text];
+    // Quit here if taskField is empty
+    if ([txt length] == 0) {
+        return; }
+    // Add it to our working array
+    [self.tasks addObject:txt];
+    // Refresh the table so that the new item shows up
+    [self.taskTable reloadData];
+    // And clear out the text field
+    [self.taskField setText:@""];
+    // Dismiss the keyboard
+    [self.taskField resignFirstResponder];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Save our tasks array to disk
+    [self.tasks writeToFile:BNRDocPath() atomically:YES];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -25,10 +153,6 @@
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
